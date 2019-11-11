d7de8940af71882bc15f8c10c1427a30696a67c9
kierepkae <kierepkaedyta@gmail.com>
2019-11-11 09:19:42 +0000

    convert string to a byte and handle error

--- Program.cs
+++ Program.cs
@@ -6,9 +6,17 @@ namespace TypeConversion {
     byte b = 1;
     int i = b;
     Console.WriteLine (i);
-    string number = '1234';
+    string number = "1234";
     int i1 = Convert.ToInt32 (number);
     Console.WriteLine (i1);
+
+    try {
+      var num = "1234";
+      byte b1 = Convert.ToByte (num);
+      Console.WriteLine (b1);
+    } catch (System.Exception) {
+      Console.WriteLine ("The number can not be converted to a byte.");
+    }
 
   }
 }
